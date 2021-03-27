56a543f07f4334a8e1fd274ab13d1aac6ce933a0
LAX1DUDE <cgiacun@gmail.com>
2021-03-27 00:59:05 -0700

    fixed line endings

--- client/test-linux.sh
+++ client/test-linux.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd windows
-java -Xmx1G -Xms1G -jar ../test.jar --debug --dev-assets ../assets_dev
+cd linux
+java -Djava.library.path=. -Xmx1G -Xms1G -jar ../test.jar --debug --dev-assets ../assets_dev
---
+++ tools/src/main/java/net/eagtek/eagl/tools/CreateColorTemps.java
@@ -0,0 +1,27 @@
+package net.eagtek.eagl.tools;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+public class CreateColorTemps {
+
+	public static void main(String[] args) throws IOException {
+		List<String> lines = FileUtils.readLines(new File("colorTemperature.txt"), Charset.forName("UTF8"));
+		FileOutputStream fos = new FileOutputStream(new File("temperatures.bin"));
+		for(String s : lines) {
+			String[] s2 = s.split("\s+");
+			if(s2[3].equals("10deg")) {
+				System.out.println(s2[10] + " " + s2[11] + " " + s2[12]);
+				fos.write((byte)Integer.parseInt(s2[10]));
+				fos.write((byte)Integer.parseInt(s2[11]));
+				fos.write((byte)Integer.parseInt(s2[12]));
+			}
+		}
+	}
+
+}
